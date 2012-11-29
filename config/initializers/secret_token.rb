227706d63742c793c1d1ca30a437c310c019670c
erlon <erlonlon@gmail.com>
2012-11-29 17:45:09 -0300

    deploy aplicacao inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xote7::Application.config.secret_token = '93152a4be03641728a02b0387cca4c1e610622c9f723e7944fe82ca12ee612b6015af5660d00bcb37e42790420267c6ea3e28cf50e8b1c7db54c72522dafd040'
