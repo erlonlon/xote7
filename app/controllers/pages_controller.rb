ad8de385a1c2a48499ab89428855503f2d77063e
erlon <erlonlon@gmail.com>
2013-03-04 07:55:45 -0300

    audio

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,8 +10,9 @@ before_filter :load_resources
    def load_resources
      @post_agenda = Category.find(4)
      @gallery_show = Type.find(1)
-    @galleries = Gallery.find(:all, :limit => 7, :order=> 'created_at desc')
+     @galleries = Gallery.find(:all, :limit => 7, :order=> 'created_at desc')
 
-    @gallery_parceiros = Type.find(4)
+     @gallery_parceiros = Type.find(4)
+     @post_noticias = Category.find(1)
    end
 end
